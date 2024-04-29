0c9103d7cda9098b2ca28cc3bcb7452a5ddc9857
KashifKhn <kashifkhnx04@gmail.com>
2024-04-29 15:10:10 +0500

    adding logic for api fetching

--- Quiz App/Js/index.js
+++ Quiz App/Js/index.js
@@ -1,13 +1,9 @@
     document.getElementById('start-button').addEventListener('click', function() {
-      // Retrieve the form values
       const category = document.getElementById('category').value;
       const difficulty = document.getElementById('difficulty').value;
       const type = document.getElementById('type').value;
       const amount = document.getElementById('amount').value;
 
-      // Construct the URL for the quiz app
       const quizAppUrl = `question.html?category=${category}&difficulty=${difficulty}&type=${type}&amount=${amount}`;
-
-      // Redirect the user to the quiz app
       window.location.href = quizAppUrl;
     });
